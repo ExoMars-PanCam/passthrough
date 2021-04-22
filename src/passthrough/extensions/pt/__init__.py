b49bc53b01f7242fab2cac56ef07e754366b9557
Ariel Ladegaard <legendin@gmail.com>
2021-04-22 20:31:07 +0100

    feat!: rename dt funcs; implement pt:datetime.now
    Let PDSDatetime default to utcnow if a timestamp is not provided; use
    this to implement pt:datetime.now.
    
    Fix minor issue with handling of decimal places.
    
    BREAKING CHANGE: adopt fake XPath scoping for datetime function names.
    (pt:datetime_inc -> pt:datetime.add)
    (pt:datetime_now -> pt:datetime.now)

--- src/passthrough/extensions/pt/__init__.py
+++ src/passthrough/extensions/pt/__init__.py
@@ -1,13 +1,11 @@
 from .context import get_context
-from .datetime import datetime_inc, datetime_now
+from .datetime import datetime_add, datetime_now
 from .misc import self, sequence
 
 functions = {
     "context": get_context,
     self.__name__: self,
     sequence.__name__: sequence,
-    # datetime_inc.__name__.replace("_", "."): datetime_inc,
-    # datetime_now.__name__.replace("_", "."): datetime_now,
-    datetime_inc.__name__: datetime_inc,
-    datetime_now.__name__: datetime_now,
+    datetime_add.__name__.replace("_", "."): datetime_add,
+    datetime_now.__name__.replace("_", "."): datetime_now,
 }
