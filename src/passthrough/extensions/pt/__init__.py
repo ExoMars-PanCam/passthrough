1ba163a79c48fed8ee0bdc44af881006335a3d16
Ariel Ladegaard <legendin@gmail.com>
2021-04-22 21:02:09 +0100

    feat: add scoped alias pt:context.get
    Allow pd:context.get to serve as an alias of pt:context.
    
    Don't cast fetched values to string; leave that to the caller.
    
    Provide commented out context setter implementation. No sane usecase
    at present, but might come in handy in the future...

--- src/passthrough/extensions/pt/__init__.py
+++ src/passthrough/extensions/pt/__init__.py
@@ -1,9 +1,11 @@
-from .context import get_context
+from .context import context_get  # , context_set
 from .datetime import datetime_add, datetime_now
 from .misc import self, sequence
 
 functions = {
-    "context": get_context,
+    "context": context_get,  # TODO: consider deprecating if pt:context.set() adopted?
+    context_get.__name__.replace("_", "."): context_get,
+    # context_set.__name__.replace("_", "."): context_set,
     self.__name__: self,
     sequence.__name__: sequence,
     datetime_add.__name__.replace("_", "."): datetime_add,
--- src/passthrough/extensions/pt/context.py
+++ src/passthrough/extensions/pt/context.py
@@ -1,4 +1,6 @@
-from collections import UserDict
+from typing import Union
+
+from lxml import etree
 
 from ...exc import PTEvalError
 
@@ -10,9 +12,23 @@ def set_context_map(map_: dict):
     _context_map = map_
 
 
-def get_context(t_elem, _, key):
+def context_get(t_elem, _, key):
     global _context_map
+    key = _unpack(key)
     try:
-        return str(_context_map[key])
+        return _context_map[key]
     except KeyError:
         raise PTEvalError(f"context entry '{key}' has not been registered", t_elem)
+
+
+# def context_set(_, __, key, value):
+#     global _context_map
+#     key = _unpack(key)
+#     _context_map[key] = value
+
+
+# TODO: should probably centralise this
+def _unpack(elem: Union[str, etree._Element]):
+    if isinstance(elem, etree._Element):
+        return elem[0].text
+    return elem
