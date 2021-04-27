2db0ab985a088c1e78060eb349dc2f6cfff6d876
Ariel Ladegaard <legendin@gmail.com>
2021-04-27 23:20:21 +0100

    feat: add exm lid subunit and time ext funcs
    Also move lid_to_browse to the lid.x XPath scope

--- src/passthrough/extensions/exm/__init__.py
+++ src/passthrough/extensions/exm/__init__.py
@@ -1,5 +1,13 @@
-from .lid import lid_to_browse
+ATTR_PATHS_EXM = {
+    "cam": "//psa:Sub-Instrument/psa:identifier",
+    "filter": "//img:Optical_Filter/img:filter_number",
+    "type": "//msn:Mission_Information/msn:product_type_name",
+}
+
+from .lid import lid_subunit, lid_time, lid_to_browse
 
 functions = {
-    lid_to_browse.__name__: lid_to_browse,
+    lid_to_browse.__name__.replace("_", ".", 1): lid_to_browse,
+    lid_subunit.__name__.replace("_", ".", 1): lid_subunit,
+    lid_time.__name__.replace("_", ".", 1): lid_time,
 }
