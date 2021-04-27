f2f2af4e61f4e3df688e55f933a73bac47dc8636
Ariel Ladegaard <legendin@gmail.com>
2021-04-27 23:15:05 +0100

    fix: handling of pt:sequence node-set arguments
    Ensure pt:sequence unwraps single-member node-sets, and complains in the
    case of empty of multi-member node-sets (see #10)

--- src/passthrough/extensions/pt/misc.py
+++ src/passthrough/extensions/pt/misc.py
@@ -1,6 +1,27 @@
-def self(t_elem, _):
-    return t_elem
+from ...exc import PTEvalError
 
 
-def sequence(_, __, *args):
-    return args
+def self(ctx):
+    return ctx.t_elem
+
+
+def sequence(ctx, *args):
+    ret = []
+    for i, arg in enumerate(args):
+        i += 1  # just used for arg num feedback
+        if isinstance(arg, list):
+            if len(arg) == 0:
+                raise PTEvalError(
+                    f"pt:sequence, argument {i}: empty node-set encountered", ctx.t_elem
+                )
+            elif len(arg) > 1:
+                raise PTEvalError(
+                    f"pt:sequence, argument {i}: node-set '{arg}' with {len(arg)}"
+                    " members cannot be added to sequence as XPath does not permit"
+                    " nested node-sets",
+                    ctx.t_elem,
+                )
+            ret.append(arg[0])
+        else:
+            ret.append(arg)
+    return ret
